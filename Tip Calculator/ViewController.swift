3514e2d4dc19bbb1de5db362409c0c93efe96170
Sandro Padin <sandropadin@gmail.com>
2014-06-19 10:50:45 -0500

    App works. Looks bad.

--- Tip Calculator/ViewController.swift
+++ Tip Calculator/ViewController.swift
@@ -9,17 +9,65 @@
 import UIKit
 
 class ViewController: UIViewController {
+  @IBOutlet var totalTextField : UITextField
+  @IBOutlet var taxPercentageLabel : UILabel
+  @IBOutlet var taxPercentageSlider : UISlider
+  @IBOutlet var resultsTextView : UITextView
+  
+  var percentage : CFloat = 0.0
+  var billTotal : CFloat = 0.0
                             
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    setup()
+    updateResultsTexView()
   }
 
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
   }
-
-
+  
+  @IBAction func taxPercentageChanged(sender : AnyObject) {
+    updatePercentage()
+    updatePercentageLabel()
+    updateResultsTexView()
+  }
+  
+  @IBAction func billTotalChanged(sender : AnyObject) {
+    updateBillTotal()
+    updateResultsTexView()
+  }
+  
+  func updateResultsTexView() {
+    let results = billTotal * (1 + (percentage/100))
+    resultsTextView.text = NSString(format:"%.2f", results)
+  }
+  
+  func setup() {
+    setupResultsTextView()
+    setupTaxPercentageSlider()
+  }
+  
+  func setupTaxPercentageSlider() {
+    taxPercentageSlider.setThumbImage(UIImage(named: "sliderhandle"), forState: UIControlState.Normal)
+    taxPercentageSlider.setThumbImage(UIImage(named: "sliderhandle"), forState: UIControlState.Highlighted)
+  }
+  
+  func setupResultsTextView() {
+    resultsTextView.selectable = false
+  }
+  
+  func updateBillTotal() {
+    billTotal = totalTextField.text.bridgeToObjectiveC().floatValue
+  }
+  
+  func updatePercentage() {
+    percentage = roundf(taxPercentageSlider.value * 2.0) / 2.0
+  }
+  
+  func updatePercentageLabel() {
+    let percentageStr = NSString(format: "%.2f", percentage)
+    taxPercentageLabel.text = "Tax Percentage (\(percentageStr)%):"
+  }
 }
 
